becbad10a6e4c30f29c6ba7b678359a0fd5dba85
felix <felix@alchemain.com>
2026-07-15 03:19:01 +0000

    chore: upgrade org.springframework.boot:spring-boot-starter-web to 4.1.0

--- src/main/java/io/zeebe/monitor/config/KafkaConfig.java
+++ src/main/java/io/zeebe/monitor/config/KafkaConfig.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
